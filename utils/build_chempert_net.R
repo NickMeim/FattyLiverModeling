e7131acaf697bf2ccbb176b09d505f4d09ad30be
Jose L. Cadavid <joseluis.cadavidcardenas@mail.utoronto.ca>
2024-04-18 10:43:19 -0400

    Create build_chempert_net.R

---
+++ utils/build_chempert_net.R
@@ -0,0 +1,35 @@
+tf_responses <- readRDS("E:/Jose Luis/Downloads/Transcriptional_responses.rds")
+colnames(tf_responses)[1] <- "Response_ID"
+metadata <- read.csv("E:/Jose Luis/Downloads/Information_for_transcriptional_responses.csv")
+
+metadata_human <- metadata %>% filter(Species == "Human" & grepl("Hepatocy", Cell_Source))
+tf_responses_hep <- tf_responses %>% filter(Response_ID %in% metadata_human$Response_ID)
+
+resp_net <- NULL
+for (ii in 1:nrow(tf_responses_hep)){
+  target <- strsplit(tf_responses_hep$`Up-regulation`[ii], 
+                     split = "; ", fixed = T)  %>% unlist() %>% unique()
+  if (length(target) > 0){
+    dummy <- data.frame(source = tf_responses_hep$Response_ID[ii],
+                        target = target,
+                        mor = 1)
+  } else {
+    dummy <- NULL
+  }
+  
+  target <- strsplit(tf_responses_hep$`Down-regulation`[ii], 
+                     split = "; ", fixed = T)  %>% unlist() %>% unique()
+  if (length(target) > 0){
+    dummy <- rbind(dummy,
+                   data.frame(source = tf_responses_hep$Response_ID[ii],
+                              target = target,
+                              mor = -1))
+  }
+  
+  resp_net <- rbind(resp_net, dummy)
+}
+
+resp_net <- resp_net %>% unique() %>% mutate(Response_ID = source)
+resp_net <- merge(resp_net, metadata_human, by = "Response_ID")
+
+save(resp_net, file = "chempert_response_network.RData")
