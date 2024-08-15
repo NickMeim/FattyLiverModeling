28d706c4be10316ae5ff376432aa4284c3dfadca
Jose L. Cadavid <joseluis.cadavidcardenas@mail.utoronto.ca>
2024-08-15 14:29:57 -0400

    Create MakeFigure2.R
    Initial commit

---
+++ figures/MakeFigure2.R
@@ -0,0 +1,17 @@
+### Functions for making elements of figure 2 - PLSR model
+### This function will load results from the pipeline to generate plots.
+### These will be specific for the chosen datasets
+
+root_dir <- "E:/Jose Luis/Documents/GitHub/FattyLiverModeling"
+setwd(root_dir)
+source("./utils/plotting_functions.R")
+
+# Read data
+
+
+
+
+
+
+# Panel - human phenotypes correlate
+plt_pheno_cor <- ggplot(data.frame(Yh), aes())
