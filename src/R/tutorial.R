bde1c200523f26633cac2e90d682a7985709b20c
NickMeim <nmeimetis97@gmail.com>
2024-08-19 04:42:58 -0400

    fix visualization, radial plots, tutorial TCs and extra, and preprocessing

--- src/R/tutorial.R
+++ src/R/tutorial.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(LIVIVTRA)
+library(AnnotationDbi)
+library(org.Hs.eg.db)
 # Function for loading datasets and appending them to a list
 load_datasets <- function(dataset_names, dir_data){
   # Look at files in data directory
@@ -58,6 +60,53 @@ Wm <- data_list[[target_dataset]]$Wm_group %>% as.matrix()
 
 ## Run main function with all the data---------------
 livivtra_results <- livivtra_run(Xh,Yh,Xm,Wm)
+W_translatable <- livivtra_results$W_translatable
+W_opt <- livivtra_results$W_opt
+W_tot <- livivtra_results$W_tot
+
+### Visualize gene loadings-------------------------
+## valta mesa sta functions ayta
+library(ggrepel)
+library(ggpubr)
+p1 <- plot_gene_loadings(loadings = W_opt,
+                             selection='V1',
+                             y_lab = 'weight in extra LV1',
+                             top=20)
+p2 <- plot_gene_loadings(loadings = W_opt,
+                         selection='V2',
+                         y_lab = 'weight in extra LV2',
+                         top=20)
+
+### Visualize pathway activity-------------------------
+pathway_interpretation_results <- pathway_activity_interpretation(W_opt,Wm)
+
+### Visualize Hallmark genesets enrichment-------------------------
+entrez_ids <- mapIds(org.Hs.eg.db, keys = rownames(W_opt), column = "ENTREZID", keytype = "SYMBOL")
+entrez_ids <- unname(entrez_ids)
+inds <- which(!is.na(entrez_ids))
+entrez_ids <- entrez_ids[inds]
+meas <- as.matrix(W_opt[inds,])
+rownames(meas) <- entrez_ids
+hallmarks_interpretation_results <- HallmarksFastenrichment(signature_ids = colnames(W_opt),
+                                                          gene_ids = rownames(meas),
+                                                          measurements = meas,
+                                                          order_columns = F, # REMOVE THAT FROM THE FUNCTION
+                                                          n_permutations=10000)
+
+p1 <- (ggplot(df_msig %>% filter(LV=='V1') %>% arrange(NES) %>%
+                filter(padj<=0.1),
+              aes(x=NES,y=reorder(Hallmark,-NES),fill=NES))+
+         geom_bar(stat = 'identity') +
+         # scale_fill_gradient(trans='log10',low = "red",high = "white",limits = c(min(df_msig$padj),1)) +
+         scale_fill_gradient2(low='darkblue',high = 'indianred',mid = 'whitesmoke',midpoint = 0)+
+         xlab('Normalized Enrichment Score') + ylab('Hallmark')+
+         ggtitle('Hallmarks enriched in LV extra 1')+
+         theme_minimal(base_family = 'Arial',base_size = 18)+
+         theme(text = element_text(family = 'Arial',size=18),
+               axis.text.y = element_text(size=18),
+               plot.title = element_text(hjust = 0.5),
+               legend.key.size = unit(1.5, "lines"),
+               legend.position = 'none'))
 
 ## Run cross validation---------------
 livivtra_CV_results <- livivtra_run_CV(Xh,Yh,Xm,Wm,num_folds = 10)
