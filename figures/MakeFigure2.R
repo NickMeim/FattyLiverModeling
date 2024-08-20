4c68b706febf3880712e10e9fa27d571dce6de32
Jose L. Cadavid <joseluis.cadavidcardenas@mail.utoronto.ca>
2024-08-20 13:42:36 -0400

    Update MakeFigure2.R
    Update aesthetics

--- figures/MakeFigure2.R
+++ figures/MakeFigure2.R
@@ -9,7 +9,7 @@ source("./utils/plotting_functions.R")
 #################################################################################
 ### Panel - human phenotypes correlate
 plt_pheno_cor <- ggplot(data.frame(Yh), aes(x = NAS, y = fibrosis)) +
-                  geom_point(size = 2*size_dot, alpha = 0.2, color = "steelblue") +
+                  geom_point(size = 2*size_dot, shape = 21, fill = "steelblue", stroke = 0.1*size_stroke, color = "black", alpha = 0.2) +
                   labs(x = "MAFLD score (MAS)", y = "Fibrosis stage")
 plt_pheno_cor <- add_theme(plt_pheno_cor)
 plt_pheno_cor <- ggMarginal(plt_pheno_cor, type = "histogram", fill = "steelblue")
@@ -22,9 +22,9 @@ performance_PLSR <- performance_PLSR %>% filter(metric=='r') %>% select(-metric)
 performance_PLSR$type <- factor(performance_PLSR$type ,levels=c('train','test','shuffle Y','shuffle X','random X'))
 performance_PLSR <- performance_PLSR %>% mutate(phenotype = ifelse(phenotype=='fibrosis','Fibrosis stage',phenotype))
 
-plt_PLSR_CV <- ggplot(performance_PLSR, aes(x = type, y = value, color = type)) +
+plt_PLSR_CV <- ggplot(performance_PLSR, aes(x = type, y = value, color = type, fill = type)) +
                 geom_boxplot(outlier.alpha = 0, size = size_line*0.7, fill = "white", show.legend = F) +
-                geom_jitter(width = 0.1, size = size_dot, show.legend = F) +
+                geom_jitter(width = 0.1, size = size_dot, shape = 21, color = "black", stroke = size_stroke, show.legend = F) +
                 stat_compare_means(comparisons = list(c('train','test'),
                                                       c('test','shuffle Y'),
                                                       c('test','shuffle X'),
@@ -36,7 +36,9 @@ plt_PLSR_CV <- ggplot(performance_PLSR, aes(x = type, y = value, color = type)) 
                 facet_wrap(~phenotype, nrow = 2) +
                 labs(x = NULL, y = "Pearson correlation") +
                 scale_y_continuous(breaks = seq(-0.75,1,0.25),limits = c(NA,1.3)) +
-                scale_color_brewer(palette = "Dark2")
+                scale_color_brewer(palette = "Dark2") +
+                scale_fill_brewer(palette = "Dark2")
+
 plt_PLSR_CV <- add_theme(plt_PLSR_CV)
 
 #################################################################################
@@ -44,7 +46,7 @@ plt_PLSR_CV <- add_theme(plt_PLSR_CV)
 plt_PLSR_training <- rbind(data.frame(Measured = Yh[,1], Phenotype = "MAS"), data.frame(Measured = Yh[,2], Phenotype = "Fibrosis stage")) %>%
                       mutate(Predicted = c(predict(plsr_model)[,1], predict(plsr_model)[,2])) %>%
                     ggplot(aes(x = Measured, y = Predicted)) +
-                      geom_jitter(size = size_dot*0.8, color = "steelblue", width = 0.1, alpha = 0.8) +
+                      geom_jitter(size = size_dot*0.8, shape = 21, fill = "steelblue", color = "black", stroke = size_stroke, width = 0.1) +
                       geom_abline(slope = 1, intercept = 0, color = "black", linewidth = size_line, linetype = 2) +
                       stat_cor(color = "black", size = size_annotation*0.7) +
                       facet_wrap(~Phenotype, scales = "free") +
@@ -53,9 +55,9 @@ plt_PLSR_training <- add_theme(plt_PLSR_training)
   
 #################################################################################
 ### Panel - PLSR scores of human data
-plt_PLSR_human <- ggplot(invivo_plsr, aes(x=V1,y=V2,color=normed_score)) +
-                  geom_point(size = size_dot)+
-                  scale_color_viridis_c()+
+plt_PLSR_human <- ggplot(invivo_plsr, aes(x=V1,y=V2,fill=normed_score)) +
+                  geom_point(size = size_dot, shape = 21, color = "black", stroke = size_stroke)+
+                  scale_fill_viridis_c()+
                   labs(color = 'Score')+
                   xlab(paste0('Human LV1 (',lv_vars[1],'%)')) + ylab(paste0('Human LV2 (',lv_vars[2],'%)')) +
                   facet_wrap(~phenotype)
